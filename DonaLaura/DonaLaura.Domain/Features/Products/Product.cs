bf76be9c6053515486e08aafb6daa3568b27d479
Isabel <isabeltosetto@gmail.com>
2018-05-13 17:56:05 -0300

    Criação das entidades Product e Sale

--- DonaLaura/DonaLaura.Domain/Features/Products/Product.cs
+++ DonaLaura/DonaLaura.Domain/Features/Products/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DonaLaura.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace DonaLaura.Domain.Features.Products
 {
-    public class Product
+    public class Product : Entity
     {
+        private string Name { get; set; }
+        private double SalePrice { get; set; }
+        private double PriceCusto { get; set; }
+        private bool Disponibility { get; set; }
+        private DateTime FabricationDate { get; set; }
+        private DateTime ValidateDate { get; set; }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DonaLaura/DonaLaura.Domain/Features/Sales/Sale.cs
+++ DonaLaura/DonaLaura.Domain/Features/Sales/Sale.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DonaLaura.Domain.Common;
+using DonaLaura.Domain.Features.Products;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace DonaLaura.Domain.Features.Sales
 {
-    public class Sale
+    public class Sale : Entity
     {
+        private Product Product { get; set; }
+        private string ClientName { get; set; }
+        private int Quantity { get; set; }
+        private double Lucre { get; set; }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
