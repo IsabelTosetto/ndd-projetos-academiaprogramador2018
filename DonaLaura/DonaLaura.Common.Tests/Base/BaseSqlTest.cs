adef59e8f068c63f84ff50a71b436881029dc811
Isabel <isabeltosetto@gmail.com>
2018-05-13 23:05:06 -0300

    Criação da classe BaseSqlTest e do script do Banco de Dados DBDonaLaura

---
+++ DonaLaura/DonaLaura.Common.Tests/Base/BaseSqlTest.cs
@@ -0,0 +1,22 @@
+﻿using DonaLaura.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Common.Tests.Base
+{
+    public static class BaseSqlTest
+    {
+        private const string RECREATE_POST_TABLE = "TRUNCATE TABLE [dbo].[TBProduct]";
+        private const string INSERT_POST = "INSERT INTO TBProduct(Name,SalePrice,CostPrice,Disponibility,FabricationDate,ExpirationDate) " +
+            "VALUES ('Produto Teste', 6, 4, 1, GETDATE(), GETDATE())";
+
+        public static void SeedDatabase()
+        {
+            Db.Update(RECREATE_POST_TABLE);
+            Db.Update(INSERT_POST);
+        }
+    }
+}
---
+++ DonaLaura/DonaLaura.Common.Tests/BdScript.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE DBDonaLaura;
+
+USE DBDonaLaura;
+
+CREATE TABLE [dbo].[TBProduct]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(40) NOT NULL,
+	[SalePrice] NUMERIC(7,2) NOT NULL,
+	[CostPrice] NUMERIC(7,2) NOT NULL,
+	[Disponibility] BIT NOT NULL,
+	[FabricationDate] DATE NOT NULL,
+	[ExpirationDate] DATE NOT NULL
+)
+
+CREATE TABLE [dbo].[TBSale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[ProductId] INT NOT NULL,
+	[ClientName] VARCHAR(40) NOT NULL,
+	[Quantity] INT NOT NULL,
+	[Lucre] NUMERIC(7,2) NOT NULL,
+	CONSTRAINT [FK_TBSale_TBProduct] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[TBProduct] ([Id])
+)
